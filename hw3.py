f949dd174ca05fb15fb7925f27f621759b76c21c
Rayan Azhari <RayanAzhari@outlook.com>
2022-08-15 01:07:58 -0400

    My third project!

---
+++ hw3.py
@@ -0,0 +1,18 @@
+
+
+print ("Hello there, please enter the a of the quadratic equation")
+a = int(input())
+print ("Please enter the b")
+b = int(input())
+print ("Finally, please enter the c")
+c = int(input())
+
+
+
+d = int(b**2) - int(4*a*c)
+
+sol1 = int(-b-(d))/(2*a)
+sol2 = int(-b+(d))/(2*a)
+
+print(sol1)
+print(sol2)
